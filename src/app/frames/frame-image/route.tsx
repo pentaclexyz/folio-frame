b7ce2c16b472887da5e8bb0bb2933bfbc0c1711f
pentcle <pentacle@pentacle.xyz>
2024-09-23 14:44:51 +0100

    feat: img

--- src/app/frames/frame-image/route.tsx
+++ src/app/frames/frame-image/route.tsx
@@ -20,7 +20,7 @@ export const GET = async (request: NextRequest) => {
     ];
 
     let content;
-    let imageSrc = `${process.env.NEXT_PUBLIC_HOST}/home-frame.png`; // Default image for 'home'
+    let imageSrc = `${process.env.NEXT_PUBLIC_HOST}/home-image.png`;
 
     switch (state) {
         case 'home':
