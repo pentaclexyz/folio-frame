ab876ec8ffb2b864db10db2b51f1fac01063bac5
pentcle <pentacle@pentacle.xyz>
2024-09-23 15:14:14 +0100

    feat: img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import { fetchMetadata } from "frames.js/next";
 
 export async function generateMetadata() {
     return {
-        title: "My page",
+        title: "folio frame",
         other: {
             ...(await fetchMetadata(
                 new URL(
                     "/frames",
                     process.env.NEXT_PUBLIC_HOST // no idea why this is like this but it works now
-                        ? "https://folio-frame.vercel.app/"
+                        ? "https://folio-frames.vercel.app/"
                         : "http://localhost:3000"
                 )
             )),
