8f4b7a1ca2cb21c56a1440d5f79d6f5e417503f2
pentcle <pentacle@pentacle.xyz>
2024-09-23 13:34:11 +0100

    feat: try to fix prod

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,13 +25,6 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <head>
-            <meta property="og:title" content="Pentafolio"/>
-            <meta property="fc:frame:post_url" content="https://folio-frame.vercel.app/"/>
-            <meta property="fc:frame" content="" />
-            <meta property="fc:frame:image" content="https://folio-frame.vercel.app/home-frame.png"/>
-            <meta property="og:image" content="https://folio-frame.vercel.app/home-frame.png"/>
-        </head>
         <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
