95f0631d6623cdd7dbed2604d0e276cc77ebc02c
pentcle <pentacle@pentacle.xyz>
2024-09-25 09:48:18 +0100

    feat: hmm did I break the url?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,17 @@
 import { fetchMetadata } from "frames.js/next";
 
 export async function generateMetadata() {
+    const baseUrl = process.env.NEXT_PUBLIC_HOST || 'http://localhost:3000';
+    const metadataUrl = new URL('/frames', baseUrl);
+
     return {
         title: "folio frame",
         other: {
-            ...(await fetchMetadata(
-                new URL(
-                    "/frames",
-                    process.env.NEXT_PUBLIC_HOST // no idea why this is like this but it works now
-                        ? "https://folio-frame2.vercel.app/"
-                        : "http://localhost:3000"
-                )
-            )),
+            ...(await fetchMetadata(metadataUrl)),
         },
     };
 }
 
 export default function Page() {
-    return <span className={"bg-pink-300 h-full p-40 flex items-center justify-items-center"}>Hello</span>;
+    return <span className={"bg-pink-300 p-40 flex items-center justify-items-center"}>Hello</span>;
 }
