878df00e3e168d439621103aee2494f8a8d58011
pentcle <pentacle@pentacle.xyz>
2024-09-26 16:05:04 +0100

    feat: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { fetchMetadata } from "frames.js/next";
+import {fetchMetadata} from "frames.js/next";
 
 export async function generateMetadata() {
     const baseUrl = process.env.NEXT_PUBLIC_HOST || 'http://localhost:3000';
     const metadataUrl = new URL('/frames', baseUrl);
 
     return {
-        title: "folio frame",
+        title: "POP – proof of project",
         other: {
             ...(await fetchMetadata(metadataUrl)),
         },
@@ -13,5 +13,15 @@ export async function generateMetadata() {
 }
 
 export default function Page() {
-    return <span className={"bg-pink-300 p-40 flex items-center justify-items-center"}>Hello</span>;
+    return <main className={"bg-[#f3eeda] h-screen flex flex-col justify-between"}>
+        <div className={"grid justify-items-end w-full pr-24"}>
+            <img className={"w-48 pt-8"} src={"images/pop-icon.svg"} alt={"pop"}/>
+        </div>
+        <div className={"flex justify-between items-center"}>
+            <div className={"w-[400px]"}>
+                <div className={""}><img src={"images/pop-char-small.png"} alt={"pop"}/></div>
+            </div>
+            <div className={"w-96 pr-12"}><img src={"images/proof-of-project-soon.svg"} alt={"pop"}/></div>
+        </div>
+    </main>;
 }
