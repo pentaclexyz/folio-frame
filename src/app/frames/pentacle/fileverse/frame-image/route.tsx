f81c29534693b5957679cc93b164eea88e4fd175
pentcle <pentacle@pentacle.xyz>
2024-10-16 09:42:14 +0100

    feat: begin refactor

--- src/app/frames/pentacle/fileverse/frame-image/route.tsx
+++ src/app/frames/pentacle/fileverse/frame-image/route.tsx
@@ -2,42 +2,7 @@ import { NextRequest } from 'next/server';
 import { ImageResponse } from 'next/og';
 import * as fs from "node:fs/promises";
 import * as path from "node:path";
-
-async function fetchFarcasterUserInfoByHandle(handle: string): Promise<{ pfp_url: string, fid: number, username: string }> {
-    try {
-        const userResponse = await fetch(`https://api.neynar.com/v1/farcaster/user-by-username?username=${handle.replace('@', '')}`, {
-            headers: {
-                'api_key': process.env.NEYNAR_API_KEY || ''
-            }
-        });
-        const userData = await userResponse.json();
-        const fid = userData.result.user.fid;
-
-        return fetchFarcasterUserInfoByFid(fid);
-    } catch (error) {
-        console.error('Error fetching Farcaster user info by handle:', error);
-        return { pfp_url: '', fid: 0, username: '' };
-    }
-}
-
-async function fetchFarcasterUserInfoByFid(fid: number): Promise<{ pfp_url: string, fid: number, username: string }> {
-    try {
-        const infoResponse = await fetch(`https://api.neynar.com/v2/farcaster/user/bulk?fids=${fid}`, {
-            headers: {
-                'api_key': process.env.NEYNAR_API_KEY || ''
-            }
-        });
-        const infoData = await infoResponse.json();
-        return {
-            pfp_url: infoData.users[0].pfp_url,
-            fid: fid,
-            username: infoData.users[0].username
-        };
-    } catch (error) {
-        console.error('Error fetching Farcaster user info by FID:', error);
-        return { pfp_url: '', fid: 0, username: '' };
-    }
-}
+import { fetchFarcasterUserInfoByHandle, fetchFarcasterUserInfoByFid } from '../../farcasterApi';
 
 export const GET = async (request: NextRequest) => {
     const searchParams = request.nextUrl.searchParams;
