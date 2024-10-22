bb4fe2af0f6427c7c656c544a271d524c15638ef
pentcle <pentacle@pentacle.xyz>
2024-10-22 17:36:46 +0100

    feat: fix metadata issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,22 @@
-import { fetchMetadata } from "frames.js/next";
-import { useRouter } from 'next/router';
+"use client";  // This component is now a client component
 
-export async function generateMetadata({ params }) {
-    const baseUrl = process.env.NEXT_PUBLIC_HOST || 'http://localhost:3000';
-
-    // Extract project name from params
-    const project = params.project || 'wildcat';  // Default to fileverse if not specified
-    const metadataUrl = new URL(`/frames/pentacle/${project}/`, baseUrl);
-
-    return {
-        title: `POP – proof of project: ${project.charAt(0).toUpperCase() + project.slice(1)}`,
-        other: {
-            ...(await fetchMetadata(metadataUrl)),
-        },
-    };
-}
+import { useSearchParams } from 'next/navigation';
 
 export default function Page() {
-    const router = useRouter();
-    const { project } = router.query;  // Dynamically pull the project name from the URL
+    const searchParams = useSearchParams();
+    const project = searchParams.get('project') || 'wildcat';
 
     return (
         <main className={"bg-[#f3eeda] h-screen flex flex-col justify-between"}>
             <div className={"grid justify-items-end w-full pr-24"}>
-                <img className={"w-48 pt-8"} src={"images/pop-icon.svg"} alt={"pop"}/>
+                <img className={"w-48 pt-8"} src={"images/pop-icon.svg"} alt={"pop"} />
             </div>
             <div className={"flex justify-between items-center"}>
                 <div className={"w-[400px]"}>
-                    <div className={""}><img src={"images/pop-char-small.png"} alt={"pop"}/></div>
+                    <div className={""}><img src={"images/pop-char-small.png"} alt={"pop"} /></div>
                 </div>
                 <div className={"pr-12"}>
-                    <div className={"w-96"}><img src={"images/proof-of-project-soon.svg"} alt={"pop"}/></div>
+                    <div className={"w-96"}><img src={"images/proof-of-project-soon.svg"} alt={"pop"} /></div>
                 </div>
             </div>
         </main>
